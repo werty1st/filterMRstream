1e6fde24f206b828a2267b0c7074a6b58f06e0ca
werty1st <adams@werty1st.de>
2015-03-09 10:42:40 +0100

    template removed

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var fs = require('fs');
-var Handlebars = require('handlebars');
-var template = fs.readFileSync('./html_template/index.html',{encoding:"utf8"});
-
 var request = require("request")
     
 var posts = [];
@@ -22,7 +19,6 @@ function save(){
 	      console.log(err);
 	    } else {
 	      console.log("JSON saved to " + "stream.json");
-	      writeHTML(posts);
 	    }
 	}); 
 
@@ -50,7 +46,7 @@ function addFB(post){
 	    	newpost.network 	= post.network;
 	    	newpost.created_time = post.created_time;
 
-			newpost.image64 = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+			newpost.face = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
 
 			posts.push(newpost);
 			open--;
@@ -83,7 +79,7 @@ function addtw(post){
 	    	newpost.network 	= post.network;
 	    	newpost.created_time = new Date(Date.parse("Tue Feb 24 11:17:18 +0000 2015")).toISOString();
 
-			newpost.image64 = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+			newpost.face = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
 
 			posts.push(newpost);
 			open--;
@@ -120,51 +116,3 @@ request({
 
 
 
-function writeHTML(posts){
-
-	console.log("created_html");
-
-	//neues html
-	var savehtml = renderTemplate(template, posts);
-
-
-	//dateinmane
-	var fout = "posts.html";
-	var path = "./html_template";
-	var saveas = path + "/" + fout;
-
-	console.log("save template");
-	
-
-	//fs.writeSync(saveas, savehtml, {encoding:"UTF-8"});
-	fs.writeFile(saveas, savehtml, 0, { encoding: "utf8" }, function(err){
-		console.log("done");
-		//process.kill();
-	});
-}
-
-
-function renderTemplate(html,data){
-	var template = Handlebars.compile(html);
-	var text = template({items:data});
-	//return new Handlebars.SafeString(text);
-	return (text);
-}
-
-
-// db.getAttachment("_design/tweetrenderdb", "templates/"+config.version+"/rendersource.html", function(err, repl){
-// 			if (!err){
-// 				var template_html = repl.body.toString('utf8');
-// 				var target_html = renderTemplate(template_html, { code:RenderRequest.code,
-// 																  bgimageurl: RenderRequest.bgimageurl,
-// 																  size: (RenderRequest.screensize==1)?"klein":"gross"
-// 																});
-
-// 				db.saveAttachment( self.doc , 	//doc.id
-// 				{ name : 'rendersource.html',
-// 				  'Content-Type' : 'text/html;charset=utf-8',
-// 				  body : target_html
-// 				},
-// 				uploadComplete("datastore.saveRendersourceComplete"));
-// 			}
-// 		});
